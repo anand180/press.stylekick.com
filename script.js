19b053339d040626ded8bfd7bc902c504edf8d80
Anthony De Guzman <anthony.deguzm@gmail.com>
2014-09-24 16:37:05 -0400

    screenshots, responsiveness

--- script.js
+++ script.js
@@ -27,5 +27,31 @@ $(document).ready(function() {
 		$('.screen-overlay3').hide();
 		$('#dl3').hide();
 	});
-})
 
+	$('.screen4').on('mouseenter', function() {
+		$('.screen-overlay4').show();
+		$('#dl4').show();
+	});
+	$('.screen4').on('mouseleave', function() {
+		$('.screen-overlay4').hide();
+		$('#dl4').hide();
+	});
+
+	$('.screen5').on('mouseenter', function() {
+		$('.screen-overlay5').show();
+		$('#dl5').show();
+	});
+	$('.screen5').on('mouseleave', function() {
+		$('.screen-overlay5').hide();
+		$('#dl5').hide();
+	});
+
+	$('.screen6').on('mouseenter', function() {
+		$('.screen-overlay6').show();
+		$('#dl6').show();
+	});
+	$('.screen6').on('mouseleave', function() {
+		$('.screen-overlay6').hide();
+		$('#dl6').hide();
+	});
+})
